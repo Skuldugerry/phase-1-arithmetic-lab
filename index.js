a9dca8f9e1b8bfcfac529f884fa9e4df4dc9deda
Hillary Chemiron <chemiron.hillary@student.moringaschool.com>
2022-08-19 15:10:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1=2;
+let num2=31;
+const multiply = (num1*num2);
+let number=Math.random()+1;
+let random=Math.round(number)
+let num3=29;
+let num4=5;
+const mod = num3%num4;
+let max= Math.max(2,5,8,20,6);
